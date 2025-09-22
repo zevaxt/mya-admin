875cf033bf5f34befde89fcca61b3d83c690ffba
yoosorio <yoosorio@yoosorio.com>
2025-09-22 02:15:18 -0500

    table estable

--- src/components/migration/SyncRelationsTable.vue
+++ src/components/migration/SyncRelationsTable.vue
@@ -3,7 +3,9 @@
     <!-- Título y botones -->
     <div class="d-flex justify-space-between align-center mb-4">
       <div>
-        <h3 class="text-h6 text-primary font-weight-medium mb-1">Sincronizaciones de Publicaciones</h3>
+        <h3 class="text-h6 text-primary font-weight-medium mb-1">
+          Sincronizaciones de Publicaciones
+        </h3>
         <p class="text-caption text-grey">
           Relaciones de sincronización entre publicaciones de diferentes cuentas
         </p>
@@ -145,29 +147,15 @@
           <div class="text-center py-4">
             <v-icon size="large" color="grey">mdi-database-off</v-icon>
             <div class="text-subtitle-1 mt-2">No se encontraron publicaciones</div>
-            <div class="text-caption text-grey">Intenta cambiar los filtros o recargar la página</div>
+            <div class="text-caption text-grey">
+              Intenta cambiar los filtros o recargar la página
+            </div>
           </div>
         </template>
 
         <!-- Columna de ID de Publicación -->
         <template #[`item.publication_id`]="slotProps">
           <div class="d-flex align-center">
-            <v-tooltip location="top">
-              <template #activator="{ props }">
-                <v-btn
-                  v-bind="props"
-                  size="x-small"
-                  icon
-                  variant="text"
-                  color="primary"
-                  class="mr-1"
-                  @click="copyToClipboard(slotProps.item.publication_id)"
-                >
-                  <v-icon size="small">mdi-content-copy</v-icon>
-                </v-btn>
-              </template>
-              <span>Copiar ID</span>
-            </v-tooltip>
             <span>{{ slotProps.item.publication_id }}</span>
           </div>
         </template>
@@ -175,67 +163,90 @@
         <!-- Columna de Sincronizaciones Salientes -->
         <template #[`item.outgoing_syncs`]="slotProps">
           <div class="d-flex align-center">
-            <v-chip
-              :color="slotProps.item.to_syncs.length > 0 ? 'primary' : 'grey'"
-              size="small"
-              variant="outlined"
-              class="mr-2"
-            >
-              {{ slotProps.item.to_syncs.length }}
-            </v-chip>
-            <v-tooltip v-if="slotProps.item.to_syncs.length > 0" location="top">
-              <template #activator="{ props }">
-                <v-btn
-                  v-bind="props"
-                  size="x-small"
-                  icon
-                  variant="text"
-                  color="primary"
-                  @click.stop="expanded = [slotProps.item.publication_id]"
-                >
-                  <v-icon size="small">mdi-eye</v-icon>
-                </v-btn>
-              </template>
-              <span>Ver sincronizaciones salientes</span>
-            </v-tooltip>
+            <!-- Cuando hay exactamente una sincronización, mostrar el ID directamente -->
+            <div v-if="slotProps.item.to_syncs.length === 1" class="d-flex align-center">
+              <v-chip color="primary" size="small" variant="outlined" class="mr-2"> 1 </v-chip>
+              <div class="d-flex align-center">
+                <span class="text-caption text-truncate" style="max-width: 180px">
+                  {{ slotProps.item.to_syncs[0].to_sync_id }}
+                </span>
+              </div>
+            </div>
+
+            <!-- Cuando hay múltiples o ninguna sincronización -->
+            <div v-else>
+              <v-chip
+                :color="slotProps.item.to_syncs.length > 0 ? 'primary' : 'grey'"
+                size="small"
+                variant="outlined"
+                class="mr-2"
+              >
+                {{ slotProps.item.to_syncs.length }}
+              </v-chip>
+              <v-tooltip v-if="slotProps.item.to_syncs.length > 0" location="top">
+                <template #activator="{ props }">
+                  <v-btn
+                    v-bind="props"
+                    size="x-small"
+                    icon
+                    variant="text"
+                    color="primary"
+                    @click.stop="expanded = [slotProps.item.publication_id]"
+                  >
+                    <v-icon size="small">mdi-eye</v-icon>
+                  </v-btn>
+                </template>
+                <span>Ver sincronizaciones salientes</span>
+              </v-tooltip>
+            </div>
           </div>
         </template>
 
         <!-- Columna de Sincronizaciones Entrantes -->
         <template #[`item.incoming_syncs`]="slotProps">
           <div class="d-flex align-center">
-            <v-chip
-              :color="slotProps.item.from_syncs.length > 0 ? 'success' : 'grey'"
-              size="small"
-              variant="outlined"
-              class="mr-2"
-            >
-              {{ slotProps.item.from_syncs.length }}
-            </v-chip>
-            <v-tooltip v-if="slotProps.item.from_syncs.length > 0" location="top">
-              <template #activator="{ props }">
-                <v-btn
-                  v-bind="props"
-                  size="x-small"
-                  icon
-                  variant="text"
-                  color="success"
-                  @click.stop="expanded = [slotProps.item.publication_id]"
-                >
-                  <v-icon size="small">mdi-eye</v-icon>
-                </v-btn>
-              </template>
-              <span>Ver sincronizaciones entrantes</span>
-            </v-tooltip>
+            <!-- Cuando hay exactamente una sincronización, mostrar el ID directamente -->
+            <div v-if="slotProps.item.from_syncs.length === 1" class="d-flex align-center">
+              <v-chip color="success" size="small" variant="outlined" class="mr-2"> 1 </v-chip>
+              <div class="d-flex align-center">
+                <span class="text-caption text-truncate" style="max-width: 180px">
+                  {{ slotProps.item.from_syncs[0].from_publication_id }}
+                </span>
+              </div>
+            </div>
+
+            <!-- Cuando hay múltiples o ninguna sincronización -->
+            <div v-else>
+              <v-chip
+                :color="slotProps.item.from_syncs.length > 0 ? 'success' : 'grey'"
+                size="small"
+                variant="outlined"
+                class="mr-2"
+              >
+                {{ slotProps.item.from_syncs.length }}
+              </v-chip>
+              <v-tooltip v-if="slotProps.item.from_syncs.length > 0" location="top">
+                <template #activator="{ props }">
+                  <v-btn
+                    v-bind="props"
+                    size="x-small"
+                    icon
+                    variant="text"
+                    color="success"
+                    @click.stop="expanded = [slotProps.item.publication_id]"
+                  >
+                    <v-icon size="small">mdi-eye</v-icon>
+                  </v-btn>
+                </template>
+                <span>Ver sincronizaciones entrantes</span>
+              </v-tooltip>
+            </div>
           </div>
         </template>
 
         <!-- Columna de Estado -->
         <template #[`item.sync_status`]="slotProps">
-          <v-chip
-            :color="getSyncStatusColor(slotProps.item)"
-            size="small"
-          >
+          <v-chip :color="getSyncStatusColor(slotProps.item)" size="small">
             {{ getSyncStatusText(slotProps.item) }}
           </v-chip>
         </template>
@@ -285,11 +296,19 @@
                   <!-- Sincronizaciones Salientes -->
                   <v-col cols="12" md="6">
                     <div class="d-flex align-center mb-2">
-                      <v-icon size="small" color="primary" class="mr-1">mdi-arrow-right-bold</v-icon>
+                      <v-icon size="small" color="primary" class="mr-1"
+                        >mdi-arrow-right-bold</v-icon
+                      >
                       <div class="text-subtitle-2 font-weight-medium">Sincroniza hacia</div>
-                      <v-chip size="x-small" color="primary" class="ml-2">{{ slotProps.item.to_syncs.length }}</v-chip>
+                      <v-chip size="x-small" color="primary" class="ml-2">{{
+                        slotProps.item.to_syncs.length
+                      }}</v-chip>
                     </div>
-                    <v-table v-if="slotProps.item.to_syncs.length > 0" density="compact" class="border rounded">
+                    <v-table
+                      v-if="slotProps.item.to_syncs.length > 0"
+                      density="compact"
+                      class="border rounded"
+                    >
                       <thead>
                         <tr>
                           <th>ID Publicación</th>
@@ -321,7 +340,12 @@
                             </div>
                           </td>
                           <td>
-                            <v-chip size="small" color="primary" variant="flat" class="font-weight-medium">
+                            <v-chip
+                              size="small"
+                              color="primary"
+                              variant="flat"
+                              class="font-weight-medium"
+                            >
                               {{ getAccountName(sync.to_account_id) }}
                             </v-chip>
                           </td>
@@ -372,9 +396,15 @@
                     <div class="d-flex align-center mb-2">
                       <v-icon size="small" color="success" class="mr-1">mdi-arrow-left-bold</v-icon>
                       <div class="text-subtitle-2 font-weight-medium">Sincronizada desde</div>
-                      <v-chip size="x-small" color="success" class="ml-2">{{ slotProps.item.from_syncs.length }}</v-chip>
+                      <v-chip size="x-small" color="success" class="ml-2">{{
+                        slotProps.item.from_syncs.length
+                      }}</v-chip>
                     </div>
-                    <v-table v-if="slotProps.item.from_syncs.length > 0" density="compact" class="border rounded">
+                    <v-table
+                      v-if="slotProps.item.from_syncs.length > 0"
+                      density="compact"
+                      class="border rounded"
+                    >
                       <thead>
                         <tr>
                           <th>ID Publicación</th>
@@ -383,7 +413,10 @@
                         </tr>
                       </thead>
                       <tbody>
-                        <tr v-for="sync in slotProps.item.from_syncs" :key="sync.from_publication_id">
+                        <tr
+                          v-for="sync in slotProps.item.from_syncs"
+                          :key="sync.from_publication_id"
+                        >
                           <td>
                             <div class="d-flex align-center">
                               <v-tooltip location="top">
@@ -406,7 +439,12 @@
                             </div>
                           </td>
                           <td>
-                            <v-chip size="small" color="success" variant="flat" class="font-weight-medium">
+                            <v-chip
+                              size="small"
+                              color="success"
+                              variant="flat"
+                              class="font-weight-medium"
+                            >
                               {{ getAccountName(sync.from_account_id) }}
                             </v-chip>
                           </td>
@@ -470,7 +508,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed, onMounted } from 'vue'
+import { ref, computed, onMounted, watch } from 'vue'
 import { useAccountStore } from '@/stores/account'
 import migrationService from '@/services/migrationService'
 import type { PublicationSyncData } from '@/services/migrationService'
@@ -497,7 +535,7 @@ const syncStatusOptions = [
   { title: 'Origen', value: 'origin' },
   { title: 'Destino', value: 'destination' },
   { title: 'Ambos', value: 'both' },
-  { title: 'Ninguno', value: 'none' }
+  { title: 'Ninguno', value: 'none' },
 ]
 
 const syncCountOptions = [
@@ -505,7 +543,7 @@ const syncCountOptions = [
   { title: 'Ninguna', value: 'none' },
   { title: '1-5', value: 'few' },
   { title: '6-10', value: 'medium' },
-  { title: 'Más de 10', value: 'many' }
+  { title: 'Más de 10', value: 'many' },
 ]
 
 // Encabezados de la tabla
@@ -514,7 +552,7 @@ const headers = [
   { title: 'Sincr. Salientes', key: 'outgoing_syncs', sortable: true },
   { title: 'Sincr. Entrantes', key: 'incoming_syncs', sortable: true },
   { title: 'Estado', key: 'sync_status', sortable: true },
-  { title: 'Acciones', key: 'actions', sortable: false }
+  { title: 'Acciones', key: 'actions', sortable: false },
 ]
 
 // Datos filtrados para la tabla
@@ -524,14 +562,12 @@ const filteredPublications = computed(() => {
   // Filtrar por búsqueda
   if (searchQuery.value) {
     const query = searchQuery.value.toLowerCase()
-    result = result.filter(item => 
-      item.publication_id.toLowerCase().includes(query)
-    )
+    result = result.filter((item) => item.publication_id.toLowerCase().includes(query))
   }
 
   // Filtrar por estado de sincronización
   if (syncStatusFilter.value !== 'all') {
-    result = result.filter(item => {
+    result = result.filter((item) => {
       const hasOutgoing = item.to_syncs.length > 0
       const hasIncoming = item.from_syncs.length > 0
 
@@ -552,7 +588,7 @@ const filteredPublications = computed(() => {
 
   // Filtrar por cantidad de sincronizaciones
   if (syncCountFilter.value !== 'all') {
-    result = result.filter(item => {
+    result = result.filter((item) => {
       const totalSyncs = item.to_syncs.length + item.from_syncs.length
 
       switch (syncCountFilter.value) {
@@ -575,26 +611,25 @@ const filteredPublications = computed(() => {
 
 // Contadores para estadísticas
 const originCount = computed(() => {
-  return publications.value.filter(item => 
-    item.to_syncs.length > 0 && item.from_syncs.length === 0
+  return publications.value.filter(
+    (item) => item.to_syncs.length > 0 && item.from_syncs.length === 0,
   ).length
 })
 
 const destinationCount = computed(() => {
-  return publications.value.filter(item => 
-    item.to_syncs.length === 0 && item.from_syncs.length > 0
+  return publications.value.filter(
+    (item) => item.to_syncs.length === 0 && item.from_syncs.length > 0,
   ).length
 })
 
 const bothCount = computed(() => {
-  return publications.value.filter(item => 
-    item.to_syncs.length > 0 && item.from_syncs.length > 0
-  ).length
+  return publications.value.filter((item) => item.to_syncs.length > 0 && item.from_syncs.length > 0)
+    .length
 })
 
 const noneCount = computed(() => {
-  return publications.value.filter(item => 
-    item.to_syncs.length === 0 && item.from_syncs.length === 0
+  return publications.value.filter(
+    (item) => item.to_syncs.length === 0 && item.from_syncs.length === 0,
   ).length
 })
 
@@ -649,17 +684,17 @@ const getSyncStatusColor = (item: PublicationSyncData) => {
 const getAccountName = (accountId: number) => {
   // Buscar en las cuentas disponibles
   const accounts = accountStore.accounts
-  const account = accounts.find(acc => acc.ID === accountId)
-  
+  const account = accounts.find((acc) => acc.ID === accountId)
+
   if (account) {
     return account.Nickname || account.Email || `Cuenta #${accountId}`
   }
-  
+
   return `Cuenta #${accountId}`
 }
 
 const viewProductDetails = (productId: string) => {
-  window.open(`/product/${productId}`, '_blank')
+  window.open(`/product-detail/${productId}`, '_blank')
 }
 
 // Usando la función importada de mercadoLibreUtils.ts
@@ -672,7 +707,7 @@ const copyToClipboard = (text: string) => {
       notificationMessage.value = 'ID copiado al portapapeles'
       notificationType.value = 'success'
     })
-    .catch(err => {
+    .catch((err) => {
       console.error('Error al copiar texto: ', err)
       showNotification.value = true
       notificationMessage.value = 'Error al copiar texto'
@@ -680,10 +715,20 @@ const copyToClipboard = (text: string) => {
     })
 }
 
-// Cargar datos al montar el componente
+// Ciclo de vida
 onMounted(() => {
   loadSyncRelations()
 })
+
+// Watcher para recargar datos cuando cambia la cuenta
+watch(
+  () => accountStore.currentAccount?.ID,
+  (newAccountId, oldAccountId) => {
+    if (newAccountId !== oldAccountId) {
+      loadSyncRelations()
+    }
+  },
+)
 </script>
 
 <style scoped>
